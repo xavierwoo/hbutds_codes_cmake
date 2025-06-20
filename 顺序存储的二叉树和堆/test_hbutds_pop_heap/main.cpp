b13343c035f49bcb15b290dc4fd80e193863a9f2
xavierwoo <xavier.woo@gmail.com>
2025-06-20 21:15:49 +0800

    测试hbutds::pop_heap

--- 顺序存储的二叉树和堆/test_hbutds_pop_heap/main.cpp
+++ 顺序存储的二叉树和堆/test_hbutds_pop_heap/main.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
 #include <format>
 #include <functional>
-#include "algorithm.h" // hbutds::push_heap所在头文件
+#include "algorithm.h" // hbutds::pop_heap所在头文件
 #include "vector.h"
 using std::cout, std::format;
 auto main() -> int {
     hbutds::vector<int> max_heap{21, 20, 12, 17, 8, 7};
-    max_heap.push_back(13);
-    hbutds::push_heap(max_heap);
+    hbutds::pop_heap(max_heap);
     for(int e : max_heap){
         cout<<format("{} ", e);
-    } // 21 20 13 17 8 7 12
+    } // 20 17 12 7 8 21
     cout<<"\n";
     hbutds::vector<int> min_heap{7, 8, 12, 17, 20, 21};
-    min_heap.push_back(5);
-    hbutds::push_heap(min_heap, std::greater<>());
+    hbutds::pop_heap(min_heap, std::greater<>());
     for(int e : min_heap){
         cout<<format("{} ", e);
-    } // 5 8 7 17 20 21 12
+    } // 8 17 12 21 20 7
     return 0;
 }
