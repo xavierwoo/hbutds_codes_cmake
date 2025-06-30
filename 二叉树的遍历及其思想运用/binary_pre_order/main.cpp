e130de67f806baa227cb16195f4f97648718ba58
xavierwoo <xavier.woo@gmail.com>
2025-06-30 18:07:34 +0800

    二叉树前序遍历递归代码测试

---
+++ 二叉树的遍历及其思想运用/binary_pre_order/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "binary_tree.h" //hbutds::binary_tree_pre_order_recur所在头文件
+
+using hbutds::BinaryTreeNode;
+
+auto main() -> int {
+    BinaryTreeNode<char> binary_tree(
+        'R',
+        new BinaryTreeNode<char>(
+            'A',
+            new BinaryTreeNode<char>('C'),
+            new BinaryTreeNode<char>('D')
+        ),
+        new BinaryTreeNode<char>(
+            'B',
+            new BinaryTreeNode<char>('E'),
+            new BinaryTreeNode<char>('F')
+        )
+    );
+
+    hbutds::binary_tree_pre_order_recur(&binary_tree);
+    return 0; // 输出 R A C D B E F
+}
