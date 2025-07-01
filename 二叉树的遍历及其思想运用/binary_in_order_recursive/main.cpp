675261ce2d6a3a5f61c192be86cec0026a51e754
xavierwoo <xavier.woo@gmail.com>
2025-07-01 21:53:38 +0800

    二叉树中序遍历测试

---
+++ 二叉树的遍历及其思想运用/binary_in_order_recursive/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "binary_tree.h" //hbutds::binary_tree_pre_order_iterative所在头文件
+
+using hbutds::BinaryTreeNode;
+
+auto main() -> int {
+    BinaryTreeNode<char> binary_tree(
+        'R',
+        new BinaryTreeNode<char>(
+            'A',
+            new BinaryTreeNode<char>('C'),
+            new BinaryTreeNode<char>('D')
+        ),
+        new BinaryTreeNode<char>(
+            'B',
+            new BinaryTreeNode<char>('E'),
+            new BinaryTreeNode<char>('F')
+        )
+    );
+
+    hbutds::binary_tree_in_order_recursive(&binary_tree);
+    return 0; // 输出 C A D R E B F
+}
