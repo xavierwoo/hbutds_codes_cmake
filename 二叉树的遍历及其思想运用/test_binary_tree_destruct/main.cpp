93c021ab025d12ad5c47c6df756dc2b0a48af72b
xavierwoo <xavier.woo@gmail.com>
2025-07-09 10:03:08 +0800

    测试二叉树节点析构

---
+++ 二叉树的遍历及其思想运用/test_binary_tree_destruct/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <format>
+#include "binary_tree.h" 
+using hbutds::BinaryTreeNode;
+using std::cout, std::format;
+struct A{
+    char data;
+    A(char d):data(d){}
+    ~A(){cout<<format("{} was destructed.\n", data);}
+};
+auto main() -> int {
+    BinaryTreeNode<A> binary_tree(
+        A('R'),
+        new BinaryTreeNode<A>(A('A')),
+        new BinaryTreeNode<A>(A('B'))
+    );
+    return 0; 
+}
