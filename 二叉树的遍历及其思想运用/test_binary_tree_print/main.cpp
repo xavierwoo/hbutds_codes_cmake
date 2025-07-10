e84a57b079e2bf4fccf7bfc6556c92f3eb96e42b
xavierwoo <xavier.woo@gmail.com>
2025-07-10 19:11:23 +0800

    打印二叉树算法测试

---
+++ 二叉树的遍历及其思想运用/test_binary_tree_print/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "binary_tree.h" //hbutds::binary_tree_print所在头文件
+using hbutds::BinaryTreeNode;
+auto main() -> int {
+    BinaryTreeNode<char> binary_tree('R',
+        new BinaryTreeNode<char>('A',
+            new BinaryTreeNode<char>('C'),
+            new BinaryTreeNode<char>('D',
+                new BinaryTreeNode<char>('G'),
+                new BinaryTreeNode<char>('H')
+            )
+        ),
+        new BinaryTreeNode<char>('B',
+            new BinaryTreeNode<char>('E'),
+            new BinaryTreeNode<char>('F')
+        )
+    );
+    hbutds::binary_tree_print(&binary_tree, 60);
+    return 0;
+}
