f6dc780a1015fbfd269d0498158aaa782036b21d
xavierwoo <xavier.woo@gmail.com>
2025-07-10 19:11:05 +0800

    二叉树层次遍历测试

---
+++ 二叉树的遍历及其思想运用/binary_level_order/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "binary_tree.h" //hbutds::binary_tree_level_order所在头文件
+using hbutds::BinaryTreeNode;
+auto main() -> int {
+    BinaryTreeNode<char> binary_tree('R',
+        new BinaryTreeNode<char>('A',
+            new BinaryTreeNode<char>('C'),
+            new BinaryTreeNode<char>('D')
+        ),
+        new BinaryTreeNode<char>('B',
+            new BinaryTreeNode<char>('E'),
+            new BinaryTreeNode<char>('F')
+        )
+    );
+    hbutds::binary_tree_level_order(&binary_tree);
+    return 0; // 输出 R A B C D E F 
+}
