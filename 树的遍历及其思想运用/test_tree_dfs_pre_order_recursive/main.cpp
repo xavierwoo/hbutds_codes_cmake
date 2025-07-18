61964d72f569d64e17060c6ba9ffc1c4bb07c91c
xavierwoo <xavier.woo@gmail.com>
2025-07-18 12:24:23 +0800

    树的深度优先 先根遍历算法测试

---
+++ 树的遍历及其思想运用/test_tree_dfs_pre_order_recursive/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "tree.h"
+
+using hbutds::TreeNode;
+
+auto main() -> int {
+    TreeNode<char> tree('A', {
+        TreeNode<char>('B', {
+            TreeNode<char>('E'),
+            TreeNode<char>('F')
+        }),
+        TreeNode<char>('C'),
+        TreeNode<char>('D', {
+            TreeNode<char>('G')
+        })
+    });
+
+    hbutds::tree_dfs_pre_order_recursive(tree);
+    return 0;
+}
