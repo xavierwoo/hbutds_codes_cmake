84123f0208fff0408b155e0e1df3578aeae4887c
xavierwoo <xavier.woo@gmail.com>
2025-08-04 13:48:59 +0800

    图删除边

---
+++ 图的基本概念和存储方法/test_graph_remove_edge/main.cpp
@@ -0,0 +1,21 @@
+#include "graph.h"
+#include <iostream>
+#include <format>
+using std::cout, std::format;
+auto main() -> int {
+    hbutds::Graph<char> graph;
+    graph.add_vertex('a');
+    graph.add_vertex('b');
+    graph.add_vertex('c');
+    graph.add_edge('a', 'b', 2);
+    graph.add_edge('a', 'c', 1);
+
+    cout<<format("Cost of edge <a, b>: {}\n", 
+            graph.get_edge_cost('a', 'b')); //输出2
+
+    graph.remove_edge('a', 'b');
+    cout<<format("Cost of edge <a, b>: {}\n", 
+            graph.get_edge_cost('a', 'b')); //输出inf
+
+    return 0;
+}
