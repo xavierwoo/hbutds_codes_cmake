d3967aafbd853b50e83740fb39a66638deb94a4e
xavierwoo <xavier.woo@gmail.com>
2025-10-23 13:02:59 +0800

    冒泡排序和堆排序

--- 基于交换的排序/test_bubble_sort/main.cpp
+++ 基于交换的排序/test_bubble_sort/main.cpp
@@ -1,11 +1,11 @@
-#include "algorithm.h" //hbutds::selection_sort所在头文件
+#include "algorithm.h" //hbutds::bubble_sort所在头文件
 #include <iostream>
 #include <format>
 #include "vector.h"
 using std::cout, std::format;
 auto main() -> int {
     hbutds::vector<int> data{41, 12, 52, 33, 41, 20, 7};
-    hbutds::selection_sort(data);
+    hbutds::bubble_sort(data);
     for(auto e : data){
         cout<<format("{} ", e);
     } // 7 12 20 33 41 41 52
