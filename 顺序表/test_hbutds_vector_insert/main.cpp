8bc38dadc2d14ee2cef6d39ef98a353ff747c3fb
xavierwoo <xavier.woo@gmail.com>
2026-02-08 16:37:40 +0800

    修改版本号

--- 顺序表/test_hbutds_vector_insert/main.cpp
+++ 顺序表/test_hbutds_vector_insert/main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <format>
-#include "vector.h"
+#include "vector.h" //hbutds::vector所在头文件
 using std::cout, std::format;
 auto main() -> int {
     hbutds::vector<int> vec{1, 2, 3, 4};
-    auto it1 {vec.insert(vec.begin(), 0)};
+    auto it1 {vec.insert(vec.begin(), 0)}; // 在初始位置插入0
     cout<<format("*it1 = {}\n", *it1); // 0
-    auto it2 {vec.insert(vec.end(), 10)};
+    auto it2 {vec.insert(vec.end(), 10)}; // 在末尾插入10
     cout<<format("*it2 = {}\n", *it2); // 10
-    auto it3 {vec.insert(vec.begin() + 2, 20)};
+    auto it3 {vec.insert(vec.begin() + 2, 20)}; // 在[2]位置插入20
     cout<<format("*it3 = {}\n", *it3); // 20
     for(int e : vec){
         cout<<format("{} ", e);
