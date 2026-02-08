b7990e71cbdf773eeabcc15a1e567d359cffe358
xavierwoo <xavier.woo@gmail.com>
2026-02-08 16:43:27 +0800

    测试reserve是否调用构造函数

---
+++ 顺序表/test_hbutds_vector_auto_construct/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "vector.h"
+struct A{
+    int a{0};
+    A(){std::cout<<"Constructor!\n";}
+};
+auto main() -> int {
+    hbutds::vector<A> vec;
+    vec.reserve(3);
+    return 0;
+}
